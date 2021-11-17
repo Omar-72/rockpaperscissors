f363cbc8923f20f43b32a250d0405700ab1adb6e
Glitch (omar-rock-paper-scissors) <none>
2021-11-17 23:28:11 +0000

    😘✈️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+$(".play").click(function(){
+  let choice= $(".input").val();
+  console.log(choice)
+  $(".userChoice").text(choice);
+});
 
+const random = Math.floor(Math.random() * 20);
+$(".result").text(random)
