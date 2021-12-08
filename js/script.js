8e6a1d4db3a173b083c0cf5a9b2b21b0bc89367f
Glitch (omar-rock-paper-scissors) <none>
2021-12-08 23:20:02 +0000

    💖☎️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -10,8 +10,20 @@ $(".play").click(function(){
     playerMove = 3;
   }
   
-  let Moves = (arr) => arr[Math.floor(Math.random()*arr.length)];
-
-let computerMove = Moves(['rock', 'paper', 'scissors'])   
-  $(".computerChoice").text(computerMove);
+  let computer = Math.ceil(Math.random()*3);
+  if (computer === 1){
+    $(".computerChoice").text("Rock");
+  } else if (computer === 2){
+    $(".computerChoice").text("Paper");
+  } else if (computer === 3){
+    $(".computerChoice").text("Scissors");
+  } 
+if (choice !== "rock") {
+  $(".userChoice").text("Enter correct responed")
+} 
 });
+
+///Or you can use this code to randomize words. 
+ ///let Moves = (arr) => arr[Math.floor(Math.random()*arr.length)];
+//let computerMove = Moves(['rock', 'paper', 'scissors'])   
+  //$(".computerChoice").text(computerMove);
